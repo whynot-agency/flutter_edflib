9b1dfc15f9c88f7fcdf6575e3d837892f118b49a
Marcis <marcis@xid.lv>
2024-10-25 10:05:35 +0300

    edf interface

---
+++ lib/src/dylib.dart
@@ -0,0 +1,24 @@
+import 'dart:ffi';
+import 'dart:io';
+
+import 'bindings.dart';
+
+EdfLib? _dylib;
+EdfLib get dylib {
+  const String libName = 'edflib';
+
+  final DynamicLibrary lib = () {
+    if (Platform.isMacOS || Platform.isIOS) {
+      return DynamicLibrary.open('$libName.framework/$libName');
+    }
+    if (Platform.isAndroid || Platform.isLinux) {
+      return DynamicLibrary.open('lib$libName.so');
+    }
+    if (Platform.isWindows) {
+      return DynamicLibrary.open('$libName.dll');
+    }
+    throw UnsupportedError('Unknown platform: ${Platform.operatingSystem}');
+  }();
+
+  return _dylib ??= EdfLib(lib);
+}
